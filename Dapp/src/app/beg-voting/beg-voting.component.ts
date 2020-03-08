8152b4b31c7c072f2a3dfad777e40e49c03238bf
sarangparikh22 <sarangparikh22@gmail.com>
2020-03-08 17:29:40 +0530

    Interfacing Panacea Age

--- Dapp/src/app/beg-voting/beg-voting.component.ts
+++ Dapp/src/app/beg-voting/beg-voting.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Router } from '@angular/router';
 
 interface Candidate {
   val: string;
@@ -19,18 +21,44 @@ export class BegVotingComponent implements OnInit {
     {val: 'congress-1', name: 'Congress'},
     {val: 'bsp-2', name: 'BSP'}
   ];
-
+  verified=false;
   voting_passwd: string='';
   voted_cdt: string ='';
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.success = false;
+    this.verified = true;
   }
 
   vote() {
     this.success=true;
     console.log(this.voted_cdt);
     console.log(this.voting_passwd)
+  }
+  vA() {
+    let url = "http://localhost:8080/age/proof";
+    this.http.get(url).subscribe(
+      res => {
+        console.log(res);
+        let url2 = "http://192.168.10.130:3000/age/verify?ageproof=" + JSON.stringify(res);
+        this.http.get(url2).subscribe(
+          res2 => {
+            console.log(res2)
+            this.verified = !res2;
+          },
+          error => {
+
+          }
+        )
+      },
+      error => {
+        console.log(error.error.text);
+
+      }
+    );
   }
 }
